c7c105bbddba1f715bb5cd8272209c425f9d9130
AKA AKA <upookluy@gmail.com>
2025-11-12 20:10:23 +0100

    dfd

--- lib/action/frends.action.ts
+++ lib/action/frends.action.ts
@@ -109,7 +109,7 @@ export const HandleThefollow = async (
     const existing = await database.listDocuments(
       process.env.DATABASE_ID as string,
       process.env.FRENDS_COLLECTION as string,
-      [Query.equal("PairId", [pairId])]
+      [Query.equal("PairId", pairId)]
     );
 
     if (existing.documents.length > 0) {
@@ -141,7 +141,7 @@ export const HandleThefollow = async (
     const receiverRes = await database.listDocuments(
       process.env.DATABASE_ID as string,
       process.env.USERS_COLLECTION as string,
-      [Query.equal("$id", [receverId])]
+      [Query.equal("$id", receverId)]
     );
 
     if (receiverRes.documents.length === 0) {
