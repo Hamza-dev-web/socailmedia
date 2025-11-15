bfca8a3bd31c6176e92d57404b0e24a5c1b33efe
AKA AKA <upookluy@gmail.com>
2025-11-15 20:24:18 +0100

    ppooo

--- app/(root)/components/userCard.tsx
+++ app/(root)/components/userCard.tsx
@@ -8,7 +8,7 @@ export const UserCard =({users , useremail , sessionuser} :{users :{$id :string 
  const [sending , setSending] =useState('Follow')
  const router = useRouter()
  const [disble , setDisable] = useState(false)
- console.log("users" , users.$id )
+ console.log("users" , sessionuser )
 return (
 <div className="flex flex-col items-center w-full mt-3" key={users.$id}>
   <div className="flex justify-between items-center bg-slate-200 w-full max-w-[1200px] px-4 py-3 rounded-xl mt-6">
