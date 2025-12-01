c110a9cf31cb1e3821bb6828563b93c10cf5e0de
AKA AKA <upookluy@gmail.com>
2025-12-01 06:13:22 +0100

    sss

--- app/(root)/[id]/messages/page.tsx
+++ app/(root)/[id]/messages/page.tsx
@@ -4,6 +4,7 @@ import { GetUserDetails, getUsers } from "@/lib/action/user.action";
 
 export default async function Home({params:{id}} :{params:{id :string}}) {
  const currentuser = await currentUser()
+ console.log(id)
  if(!currentuser) return 
  const user = await getUsers(currentuser.emailAddresses[0].emailAddress) 
 if(!user) return
